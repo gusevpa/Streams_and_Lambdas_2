a29fdce810efd0e7be992124276ca8f7954aadf7
Admin <flenivetc@gmail.com>
2021-04-17 11:46:10 +0300

    something changes...

--- src/Main.java
+++ src/Main.java
@@ -8,38 +8,39 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
-        LocalDateTime start1Time = LocalDateTime.now();
-        Set<Account> accounts = new AcccountCreator().getAccSet(1000000);
+        Set<Account> accountsAsSet = new AcccountCreator().getAccSet(1000000);
 
-        Set<Account> nonEmptyAccounts = accounts.stream()
+        LocalDateTime start1Time = LocalDateTime.now();
+        Set<Account> nonEmptyAccounts = accountsAsSet.stream()
                 .filter(p -> p.getBalance() > 0)
                 .collect(Collectors.toSet());
 
-        Set<Account> accountsWithTooMuchMoney = accounts.stream()
+        Set<Account> accountsWithTooMuchMoney = accountsAsSet.stream()
                 .filter(p -> !p.islocked())
                 .filter(p -> p.getBalance() >= 100000000)
                 .collect(Collectors.toSet());
 
-        System.out.println((double) nonEmptyAccounts.size()/accounts.size() + " vs " + (double) accountsWithTooMuchMoney.size()/accounts.size());
+        System.out.println((double) nonEmptyAccounts.size()/accountsAsSet.size() + " vs " + (double) accountsWithTooMuchMoney.size()/accountsAsSet.size());
+
         LocalDateTime finish1Time = LocalDateTime.now();
         Duration d1 = Duration.between(start1Time, finish1Time);
 
+        List<Account> accountsAsList = new ArrayList<>(accountsAsSet);
 
-        List<Account> accountList = new ArrayList<>(accounts);
         LocalDateTime start2Time = LocalDateTime.now();
-        List<Account> nonEmptyAccounts2 = accountList.stream()
+        List<Account> nonEmptyAccounts2 = accountsAsList.stream()
                 .filter(p -> p.getBalance() > 0)
                 .collect(Collectors.toList());
 
-        List<Account> accountsWithTooMuchMoney2 = accountList.stream()
+        List<Account> accountsWithTooMuchMoney2 = accountsAsList.stream()
                 .filter(p -> !p.islocked())
                 .filter(p -> p.getBalance() >= 100000000)
                 .collect(Collectors.toList());
 
-        System.out.println((double) nonEmptyAccounts2.size()/accounts.size() + " vs " + (double) accountsWithTooMuchMoney2.size()/accounts.size());
+        System.out.println((double) nonEmptyAccounts2.size()/accountsAsSet.size() + " vs " + (double) accountsWithTooMuchMoney2.size()/accountsAsSet.size());
         LocalDateTime finish2Time = LocalDateTime.now();
         Duration d2 = Duration.between(start2Time, finish2Time);
 
-        System.out.println("as Set: " + d1.getNano()/1000000 + "\nvs \nas List: " + d2.getNano()/1000000);
+        System.out.println("as Set: " + d1.getNano()/1000000 + " ms\nvs \nas List: " + d2.getNano()/1000000 + " ms \n but WHY?");
     }
 }
